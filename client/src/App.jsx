0dd32ed7ce7c9854cd3b84d8dd1b047f2342a6d7
dameine yang <dameiney123@gmail.com>
2024-08-08 15:28:50 -0500

    query.js added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,27 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import Navbar from './components/Navbar';
 
+// The ApolloClient instance is created and exported from the client/src/App.jsx file. The uri property is set to /graphql, which is the endpoint that the Apollo Client will use to make requests to the server. The cache property is set to a new instance of InMemoryCache, which will store the data returned from the server in memory.
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+
+// The ApolloClient instance is created and exported from the client/src/App.jsx file. The uri property is set to /graphql, which is the endpoint that the Apollo Client will use to make requests to the server. The cache property is set to a new instance of InMemoryCache, which will store the data returned from the server in memory.
 const client = new ApolloClient({
-  uri:'/graphql',
+  uri: '/graphql',
   cache: new InMemoryCache()
 });
 
+
+// The App component is the root of the application. It uses the ApolloProvider component to provide the Apollo Client instance to the entire application. The Navbar component is rendered at the top of the page, and the Outlet component is used to render the appropriate page based on the current route.
 function App() {
   return (
     <>
-    <ApolloProvider client={client}>
-      <Navbar />
-      <Outlet />
-    </ApolloProvider>
+      <ApolloProvider client={client}>
+        <Navbar />
+        <Outlet />
+      </ApolloProvider>
     </>
   );
 }
--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -7,12 +7,19 @@ import {
   Col
 } from 'react-bootstrap';
 
+import { useMutation, useQuery } from '@apollo/client';
+import { REMOVE_BOOK } from '../utils/mutations';
+import { QUERY_ME } from '../utils/queries';
+
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
+
+  const { loading, data } = useQuery(QUERY_ME);
+  const [removeBook] = useMutation(REMOVE_BOOK);
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
