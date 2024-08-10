a6ddb5e088a1f0dbc94b5d1f53150e280eee2009
dameine yang <dameiney123@gmail.com>
2024-08-10 12:42:12 -0500

    client update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 // The ApolloClient instance is created and exported from the client/src/App.jsx file. The uri property is set to /graphql, which is the endpoint that the Apollo Client will use to make requests to the server. The cache property is set to a new instance of InMemoryCache, which will store the data returned from the server in memory.
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 
 import {setContext} from '@apollo/client/link/context';
 
@@ -25,7 +25,7 @@ const authLink = setContext((_, { headers }) => {
 
 // The ApolloClient instance is created and exported from the client/src/App.jsx file. The uri property is set to /graphql, which is the endpoint that the Apollo Client will use to make requests to the server. The cache property is set to a new instance of InMemoryCache, which will store the data returned from the server in memory.
 const client = new ApolloClient({
-  uri: '/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
