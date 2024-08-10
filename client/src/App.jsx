cd532138711d70f500f47200556b3890fedc29ba
dameine yang <dameiney123@gmail.com>
2024-08-10 11:10:38 -0500

    client side upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,22 @@ import Navbar from './components/Navbar';
 // The ApolloClient instance is created and exported from the client/src/App.jsx file. The uri property is set to /graphql, which is the endpoint that the Apollo Client will use to make requests to the server. The cache property is set to a new instance of InMemoryCache, which will store the data returned from the server in memory.
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
+import {setContext} from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 
 // The ApolloClient instance is created and exported from the client/src/App.jsx file. The uri property is set to /graphql, which is the endpoint that the Apollo Client will use to make requests to the server. The cache property is set to a new instance of InMemoryCache, which will store the data returned from the server in memory.
 const client = new ApolloClient({
